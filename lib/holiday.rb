6a8ace6a2fbab10fb32e703c0060b3ba92adb3f6
Derek Townsend <derekjavon2@gmail.com>
2019-05-11 04:42:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,7 +28,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash.each do |key,value|
     puts key
   end
-
+end
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
