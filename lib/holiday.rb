956942e484fe5dd3d4d72e4cba774062aee50f26
Derek Townsend <derekjavon2@gmail.com>
2019-05-11 04:49:39 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -26,12 +26,10 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
   holiday_hash.values.each do |key,value|
-    puts "1 #{key}"
     if key==:winter
-
-      #  value.each do |item1,item2|
-      #    item2<<supply
-      #  end
+       value.each do |item1,item2|
+         item2<<supply
+       end
     end
   end
 end
@@ -39,13 +37,13 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-  # holiday_hash.each do |key,value|
-  #   if key==:spring
-  #      value.each do |item1,item2|
-  #        item2<<supply
-  #      end
-  #   end
-  # end
+  holiday_hash.each do |key,value|
+    if key==:spring
+       value.each do |item1,item2|
+         item2<<supply
+       end
+    end
+  end
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
