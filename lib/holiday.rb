f06dea530d37aca1b89c2ad2091a015efdba120f
Derek Townsend <derekjavon2@gmail.com>
2019-05-11 05:36:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -89,7 +89,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |key,season|
     puts "#{key.capitalize}:"
     season.values.each do |holiday_name,supply|
-      puts "#{season}:"
+      puts "#{supply}:"
       # supply.each do |item|
         # puts "#{item}"
       # end
