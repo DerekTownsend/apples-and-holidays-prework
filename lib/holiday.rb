5c86ac7e10c32ca3401f66f81b51cdc9a8bee655
Derek Townsend <derekjavon2@gmail.com>
2019-05-11 05:18:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,12 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |key,value|
+    puts "#{key}"
+    value.each do |item|
+      supplies<<item
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
