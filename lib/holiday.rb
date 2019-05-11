526106c11f4f81c29ccac6d45a502ccff8b531b5
Derek Townsend <derekjavon2@gmail.com>
2019-05-11 04:40:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,10 +28,9 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash.each do |key,value|
     puts value
     value.each do |item|
-    if key==:winter
-      value<<supply
-    end
-
+      if key==:winter
+        value<<supply
+      end
     end
   end
 end
