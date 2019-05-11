47ae44e0a9ddebd115621c2a5cde3991de6a2f3e
Derek Townsend <derekjavon2@gmail.com>
2019-05-11 05:12:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter].each do |key,value|
     supplies<<value.join(",")
   end
-  supplies.split
+  supplies.split(",")
 end
 
 def all_supplies_in_holidays(holiday_hash)
